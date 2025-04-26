8a790a855831cf4e3a0c51e21806acbcea6c01a0
Sergey <Cepaxp2008@mail.ru>
2025-04-26 13:24:21 +0300

    Update my.py

---
+++ my1.py
@@ -0,0 +1,36 @@
+# Ввод значения от пользователя и преобразование в целое число
+# a = int(input("Введите число первое: "))
+# b = int(input("Введите второе числоЖ "))
+# print("a = ", a) # вывод переменной a
+# print("b = ", b) # вывод переменной b
+# print("Сумма a+b=",a+b) # Вывод суммы a+b
+"""
+Третье занятие
+по Python
+"""
+# целые числа
+a = 5
+b = 0
+c = -10
+d = 500000
+e = 600_000
+print(a , b , c , d , e)
+
+# дробные числа (float)
+a = 3.5
+b = 0.0
+c = -5.6
+d = 50_000.000_03
+e = 4.5e2
+print(a, b , c, d, e)
+
+# логический тип данных (boolean)
+print(bool(0))
+print(bool(-1))
+print(bool(5))
+print(bool(0.0))
+print(bool(True))
+print(bool(False))
+print(bool(""))
+print(bool(" "))
+print(bool("Привет мир"))
